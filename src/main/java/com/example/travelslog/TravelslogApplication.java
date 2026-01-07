b6c7aee3585f693a04e7f2780bed876d44d84154
kwondo1017 <127649467+kwondo1017@users.noreply.github.com>
2026-01-07 19:49:45 +0900

    initialized commit

---
+++ src/main/java/com/example/travelslog/TravelslogApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelslog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelslogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelslogApplication.class, args);
+	}
+
+}
